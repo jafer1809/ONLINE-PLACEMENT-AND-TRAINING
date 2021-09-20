27b0b91454bcec9fd52de51bdefddf04ffbd4b5b
jafer1809 <mdjaferansari786@gmail.com>
2021-09-20 21:15:10 +0530

    week2

---
+++ mini-project/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>placement and training</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<h1 class="welcome"> welcome to training and placement</h1>
+
+<div class="image">
+<div class="student">
+<a href="login_student.php"><img src="student.jpg"></a>
+</div>
+<div class="company">
+<a href="login_company.php"><img src="company.jpg"</a>
+ </div>  
+</div> 
+
+    
+</body>
+</html>
